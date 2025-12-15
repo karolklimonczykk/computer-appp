bc248c88d3419512f30bf303ffa83c9e3a1faa88
Karol Klimonczyk <karol.klimonczyk@student.pk.edu.pl>
2025-12-16 00:50:34 +0100

    f

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
-import { connection } from "next/server";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-})
-const prisma = globalForPrisma.prisma || new PrismaClient({
-    adapter
+  connectionString: process.env.DATABASE_URL,
 });
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
