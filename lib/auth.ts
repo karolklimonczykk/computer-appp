b9372cc90ff4cf0ffe4289b235aed79c575bf989
Karol Klimończyk <karol.klimonczyk@student.pk.edu.pl>
2025-12-16 01:28:25 +0100

    f

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,17 +5,19 @@ import prisma from "./prisma";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET, // <— DODAJ
+  trustHost: true, // <— DODAJ (na Vercelu pomaga z host/url)
+
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
-      clientId: process.env.CLIENT_ID!,
-      clientSecret: process.env.CLIENT_SECRET!,
+      clientId: process.env.AUTH_GITHUB_ID ?? process.env.CLIENT_ID!,        // <— ZMIEŃ
+      clientSecret: process.env.AUTH_GITHUB_SECRET ?? process.env.CLIENT_SECRET!, // <— ZMIEŃ
     }),
   ],
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
-        // dopisujemy id użytkownika do obiektu sesji
         (session.user as any).id = user.id;
       }
       return session;
